762d4e45015952ddfc13bd9cc7e2a1bad0164987
lemon <986597535@qq.com>
2016-11-08 20:20:23 +0800

    'main.js(tw)'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-function() {
-	console.log(333);
+function  tangwei() {
+	console.log(333+'汤伟新加');
 }
 
 function() {
 	alert('msg');
+	document...........
+}
+
+function() {
+	new 
+}
+function() {
+	new 
+}
+function() {
+	new 
+}
+function() {
+	new 
+}
+function() {
+	new j孤家寡人
 }
