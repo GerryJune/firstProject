64c4363baa56781c008cd1d75fb6ca59de514b69
GerryJune <1042469089@qq.com>
2016-11-08 21:03:50 +0800

    gejianhui

---
+++ js/plus.js
@@ -0,0 +1,42 @@
+function dsfd() {
+	console.log(333);
+}
+
+function dsfdsf() {
+	alert('m123');
+	console.log(dasdsadsadas)
+}
+// asfsaljdasd
+function alsss3321(){
+	console.log(12213213)
+}
+// adsjlakjdlk
+function alsss122(){
+	
+}
+// dasdasdasd
+function dsfdsf12312(){
+	return "gejianhui"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// tw
+function tangwei111(arguments) {
+	
+}
+function tangwei222(arguments) {
+	
+}
+function tangwei333(arguments) {
+	
+}
